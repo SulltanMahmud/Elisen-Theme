4294f513886e12d509f40d473186ab2025c41029
SultanMahmud <smahmud191155@bscse.uiu.ac.bd>
2024-06-04 14:23:18 +0600

    nav responsive done for 1024

---
+++ js/img-splide.js
@@ -0,0 +1,52 @@
+let img = new Splide("#left-img", {
+  type: "loop",
+  perPage: 1,
+  autoplay: true,
+  arrows: false,
+  interval: 3000,
+  pagination: false,
+  gap: 20,
+
+  breakpoints: {
+    576: {
+      perPage: 1,
+    },
+
+    768: {
+      perPage: 1,
+    },
+
+    1024: {
+      perPage: 1,
+    },
+  },
+});
+
+img.mount();
+
+let card = new Splide("#card", {
+  type: "loop",
+  perPage: 2,
+  autoplay: true,
+  arrows: false,
+  interval: 3000,
+  pagination: false,
+  gap: 20,
+  perMove: 1,
+  breakpoints: {
+    576: {
+      perPage: 1,
+    },
+
+    768: {
+      perPage: 2,
+      gap: 10,
+    },
+
+    1024: {
+      perPage: 2,
+    },
+  },
+});
+
+card.mount();
